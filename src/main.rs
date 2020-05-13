55bf2694b1f943282f40a07338b74c6196db4de4
jonay2000 <jonabent@gmail.com>
2020-05-13 09:09:18 +0200

    Fixed all PR issues

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use log::*;
+use dotenv;
+
+mod api;
 
 pub fn main() {
+    // Load environment variables from .env file
+    dotenv::dotenv().unwrap();
+
     // Init program
     pretty_env_logger::init();
 
