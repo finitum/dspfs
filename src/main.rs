f9abf6cdf891c63b077c96b5d886131ee999ec82
jonay2000 <jonabent@gmail.com>
2020-08-02 11:55:33 +0200

    changed config dir to .config/dspfs

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let config_dir = env::var("DSPFS_CONFIG_DIR")
         .map(|i| i.into())
-        .or_else(|_|{
-            dirs::config_dir().context("Couldn't find default configuration directory")
+        .or_else::<anyhow::Error, _>(|_|{
+            let mut dir = dirs::config_dir().context("Couldn't find default configuration directory")?;
+            dir.push("dspfs");
+            Ok(dir)
         }).expect("Couldn't find default config location and no user override given (envvar: DSPFS_CONFIG_DIR)");
 
     log::info!("Using {:?} as configuration directory", config_dir);
